d5fdefe2ee59483210e34a5e7a3b3e61df3a0c54
mohdt-optimus <mohd.taha@optimusinfo.com>
2015-07-16 19:03:37 +0530

    Started Creating Camera APp

---
+++ Camera_app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera_app
+//
+//  Created by optimusmac-12 on 16/07/15.
+//  Copyright (c) 2015 mdtaha.optimus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Camera_app/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Camera_app
+//
+//  Created by optimusmac-12 on 16/07/15.
+//  Copyright (c) 2015 mdtaha.optimus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
