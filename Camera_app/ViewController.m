85d1030e0703506a84b764ba14e3627c537a5daa
mohdt-optimus <mohd.taha@optimusinfo.com>
2015-07-20 16:32:25 +0530

    Completed the implementation Camera Application, testing this app on device is pending

--- Camera_app/ViewController.m
+++ Camera_app/ViewController.m
@@ -17,6 +17,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        
+        UIAlertView *msg = [[UIAlertView alloc] initWithTitle:@"Attention"
+                                                              message:@"Camera Not Available"
+                                                             delegate:nil
+                                                    cancelButtonTitle:@"OK"
+                                                    otherButtonTitles: nil];
+        
+        [msg show];
+        
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +35,41 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)gallery:(id)sender
+{
+    UIImagePickerController *gallery = [[UIImagePickerController alloc] init];
+    gallery.delegate = self;
+    gallery.allowsEditing = YES;
+    gallery.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:gallery animated:YES completion:NULL];
+
+}
+
+- (IBAction)takephoto:(id)sender
+{
+    UIImagePickerController *pic=[[UIImagePickerController alloc]init];
+    pic.delegate=self;
+    pic.allowsEditing=YES;
+    pic.sourceType=UIImagePickerControllerSourceTypeCamera;
+    [self presentViewController:pic animated:YES completion:NULL];
+        
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    UIImage *Imageselected = info[UIImagePickerControllerEditedImage];
+    self.picture.image = Imageselected;
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
 @end
+
